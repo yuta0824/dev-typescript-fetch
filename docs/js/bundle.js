acb49a77c812926cf0a2e4f37d8e89013526196d
yuta <yu.code.tech@gmail.com>
2024-12-30 21:44:53 +0900

    build

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -2,14 +2,158 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/css/output.css":
-/*!****************************!*\
-  !*** ./src/css/output.css ***!
-  \****************************/
+/***/ "./src/js/user.ts":
+/*!************************!*\
+  !*** ./src/js/user.ts ***!
+  \************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   appendUserList: () => (/* binding */ appendUserList),
+/* harmony export */   fetchUserList: () => (/* binding */ fetchUserList),
+/* harmony export */   removeUsersElement: () => (/* binding */ removeUsersElement)
+/* harmony export */ });
+/* harmony import */ var _utils_dom__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils/dom */ "./src/js/utils/dom.ts");
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+
+/**
+ * ユーザー情報をAPIから取得する
+ *
+ * @returns Promise<Users[]>
+ */
+var fetchUserList = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var response, users;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, fetch("https://jsonplaceholder.typicode.com/users/")];
+            case 1:
+                response = _a.sent();
+                if (!response.ok) {
+                    throw new Error("HTTP error! status: ".concat(response.status));
+                }
+                return [4 /*yield*/, response.json()];
+            case 2:
+                users = _a.sent();
+                return [2 /*return*/, users];
+        }
+    });
+}); };
+/**
+ * ユーザー情報をDOMに追加する
+ *
+ * @param users APIで取得したユーザー情報
+ * @returns void
+ */
+var appendUserList = function (users) {
+    var userList = (0,_utils_dom__WEBPACK_IMPORTED_MODULE_0__.getElementById)("user-list");
+    users.forEach(function (user) {
+        var userElement = (0,_utils_dom__WEBPACK_IMPORTED_MODULE_0__.createElement)("li", " ".concat(user.name, " : ").concat(user.email));
+        userList.appendChild(userElement);
+    });
+};
+/**
+ * ユーザー情報を削除する
+ *
+ * @returns void
+ */
+var removeUsersElement = function () {
+    var _a;
+    var userList = (0,_utils_dom__WEBPACK_IMPORTED_MODULE_0__.getElementById)("user-list");
+    while (userList.firstChild) {
+        (_a = userList.firstChild) === null || _a === void 0 ? void 0 : _a.remove();
+    }
+};
+
+
+/***/ }),
+
+/***/ "./src/js/utils/dom.ts":
+/*!*****************************!*\
+  !*** ./src/js/utils/dom.ts ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   createElement: () => (/* binding */ createElement),
+/* harmony export */   getElementById: () => (/* binding */ getElementById),
+/* harmony export */   getInputElementById: () => (/* binding */ getInputElementById)
+/* harmony export */ });
+/**
+ * id属性からHTML要素を取得する
+ * @param id
+ * @returns HTMLElement
+ */
+var getElementById = function (id) {
+    var element = document.getElementById(id);
+    if (!element) {
+        throw new Error("".concat(id, " not found"));
+    }
+    return element;
+};
+/**
+ * id属性からHTMLInputElement要素を取得する
+ * @param id
+ * @returns HTMLInputElement
+ */
+var getInputElementById = function (id) {
+    var element = document.getElementById(id);
+    if (!element) {
+        throw new Error("".concat(id, " not found"));
+    }
+    return element;
+};
+/**
+ * HTML要素を生成する
+ *
+ * @param elementName 要素名
+ * @param textContent 要素のテキストコンテント
+ * @param className 要素のクラス属性
+ * @returns HTMLElement
+ */
+var createElement = function (elementName, textContent) {
+    var element = document.createElement(elementName);
+    if (typeof textContent !== "undefined") {
+        element.textContent = textContent;
+    }
+    return element;
+};
 
 
 /***/ })
@@ -41,6 +185,23 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
@@ -60,9 +221,61 @@ var __webpack_exports__ = {};
   !*** ./src/js/index.ts ***!
   \*************************/
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _css_output_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../css/output.css */ "./src/css/output.css");
+/* harmony import */ var _user__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./user */ "./src/js/user.ts");
+/* harmony import */ var _utils_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./utils/dom */ "./src/js/utils/dom.ts");
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 
-console.log("Hello, world!");
+
+var userButton = (0,_utils_dom__WEBPACK_IMPORTED_MODULE_1__.getElementById)("button-user");
+userButton.addEventListener("click", function () { return __awaiter(void 0, void 0, void 0, function () {
+    var users;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, (0,_user__WEBPACK_IMPORTED_MODULE_0__.fetchUserList)()];
+            case 1:
+                users = _a.sent();
+                // ユーザー情報の表示
+                (0,_user__WEBPACK_IMPORTED_MODULE_0__.removeUsersElement)();
+                (0,_user__WEBPACK_IMPORTED_MODULE_0__.appendUserList)(users);
+                return [2 /*return*/];
+        }
+    });
+}); });
 
 })();
 
